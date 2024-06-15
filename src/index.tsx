a5cbed1cce1854f3b98bd4494b7607175d597722
Degraft Frimpong <frimpsup@gmail.com>
2024-06-15 18:34:56 -0400

    currency api added

--- src/index.tsx
+++ src/index.tsx
@@ -12,3 +12,24 @@ const currencyData:CountryCurrency[]=[
     {country:"GB",currency:"GBP",symbol:''},
     {country:"EU",currency:"EUR",symbol:''},
 ]
+
+
+const getCurentcyByCountryCode=(countryCode:string):CountryCurrency|undefined=>{
+    return currencyData.find((data)=>data.country===countryCode);
+}
+
+const getUserByCountryCode=async():Promise<string>=>{
+    const response=await axios.get('http://ipapi.co/json/');
+    return response.data.country_code;
+}
+
+
+export const hetUsersCurrency=async():Promise<string>=>{
+    const countrycode=await getUserByCountryCode();
+    const currency=getCurentcyByCountryCode(countrycode);
+    if(currency){
+        return `${currency.symbol} (${currency.currency})`;
+    }else{
+        return 'Currency not found';
+    }
+}
